2631828b0d498c80a796591e76551f6da1991049
jacksonford7 <jackson.ford18@outlook.com>
2025-07-28 13:31:04 -0500

    Add ControlesAccesoQR project with main form

---
+++ ControlesAccesoQR/FrmEntradaSalida.cs
@@ -0,0 +1,14 @@
+using System.Windows.Forms;
+
+namespace RECEPTIO.ControlesAccesoQR
+{
+    public partial class FrmEntradaSalida : Form
+    {
+        public FrmEntradaSalida()
+        {
+            InitializeComponent();
+        }
+
+        // TODO: Cambiar estado del pase a 'En Curso' en la tabla [vhs].[PasePuerta]
+    }
+}
---
+++ ControlesAccesoQR/FrmSalidaFinal.cs
@@ -0,0 +1,14 @@
+using System.Windows.Forms;
+
+namespace RECEPTIO.ControlesAccesoQR
+{
+    public partial class FrmSalidaFinal : Form
+    {
+        public FrmSalidaFinal()
+        {
+            InitializeComponent();
+        }
+
+        // TODO: Cambiar estado del pase a 'Finalizado' en la tabla [vhs].[PasePuerta]
+    }
+}
